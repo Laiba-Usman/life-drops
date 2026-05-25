f32b0d618ebb2c4c391d57d5f0c0a83872156020
Laiba-Usman <laibausman3027@gmail.com>
2026-05-25 13:50:16 +0500

    Delete src directory

--- src/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-/**
- * Wraps an async route handler and forwards any errors to Express error middleware.
- * Eliminates the need for try/catch in every controller.
- */
-const asyncHandler = (fn) => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
-
-module.exports = asyncHandler;
